515aec91a60ea280ff2cab83258f36b2be534b98
Cristian Tiut <tiutcristian@gmail.com>
2025-10-16 10:44:29 +0300

    initial commit

---
+++ Ellipsoid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace rt
+{
+    public class Ellipsoid : Geometry
+    {
+        private Vector Center { get; }
+        private Vector SemiAxesLength { get; }
+        private double Radius { get; }
+        
+        
+        public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Material material, Color color) : base(material, color)
+        {
+            Center = center;
+            SemiAxesLength = semiAxesLength;
+            Radius = radius;
+        }
+
+        public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Color color) : base(color)
+        {
+            Center = center;
+            SemiAxesLength = semiAxesLength;
+            Radius = radius;
+        }
+
+        public override Intersection GetIntersection(Line line, double minDist, double maxDist)
+        {
+            // TODO: ADD CODE HERE
+            return Intersection.NONE;
+        }
+    }
+}
---
+++ RayTracer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace rt
+{
+    class RayTracer(Geometry[] geometries, Light[] lights)
+    {
+        private double ImageToViewPlane(int n, int imgSize, double viewPlaneSize)
+        {
+            return -n * viewPlaneSize / imgSize + viewPlaneSize / 2;
+        }
+
+        private Intersection FindFirstIntersection(Line ray, double minDist, double maxDist)
+        {
+            var intersection = Intersection.NONE;
+
+            foreach (var geometry in geometries)
+            {
+                var intr = geometry.GetIntersection(ray, minDist, maxDist);
+
+                if (!intr.Valid || !intr.Visible) continue;
+
+                if (!intersection.Valid || !intersection.Visible)
+                {
+                    intersection = intr;
+                }
+                else if (intr.T < intersection.T)
+                {
+                    intersection = intr;
+                }
+            }
+
+            return intersection;
+        }
+
+        private bool IsLit(Vector point, Light light)
+        {
+            // TODO: ADD CODE HERE
+            return true;
+        }
+
+        public void Render(Camera camera, int width, int height, string filename)
+        {
+            var background = new Color(0.2, 0.2, 0.2, 1.0);
+
+            var image = new Image(width, height);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    // TODO: ADD CODE HERE
+                    image.SetPixel(i, j, background);
+                }
+            }
+
+            image.Store(filename);
+        }
+    }
+}
